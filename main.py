5ba80876625f9b30812d241d0466a7bab250dd8f
evan <evanfang1013>
2021-06-16 10:34:38 +0800

    v2

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import time
 import RPi.GPIO as GPIO
 import time
+import datetime
 from FTPLIB import ftpsend
 import threading
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+#log init
+log_filename = datetime.datetime.now().strftime("./log/%Y-%m-%d_%H_%M.log")
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    handlers = [TimedRotatingFileHandler(filename =log_filename ,when="D",interval=1,backupCount=30)]
+                    )
 
 #Define ENS message file
 #define GPIO pinout
-
-Machine_PIN = 21;Machine_ENS = 'CUSOVTLeakEvent_SubGrp_20200728.txt'
+Machine1_PIN = 12;Machine1_ENS = './Event/WaterLeakEvent_M1.txt'
+Machine2_PIN = 16;Machine2_ENS = './Event/WaterLeakEvent_M2.txt'
+Machine3_PIN = 20;Machine3_ENS = './Event/WaterLeakEvent_M3.txt'
+Machine4_PIN = 21;Machine4_ENS = './Event/WaterLeakEvent_M4.txt'
 
 GPIO.setmode(GPIO.BCM)
 
@@ -20,25 +32,27 @@ def detect_water(DetectPIN,message):
 
 		if not GPIO.input(DetectPIN):
 			if not pressed:
-				if (time.time()-timetemp)>=5:
-					print(DetectPIN,"Detect water!")
-					ftpsend(message)
+				if (time.time()-timetemp)>=3:
+					logging.info("[InFo] Detect water leak : GPIO %s"%DetectPIN )
+					try:
+						ftpsend(message)
+					except:
+						logging.warning("[Warn] ENS FTP server connect fail" )
 					pressed = True	
-		# DR(or released)
+		# DR (or released)
 		else:
 			timetemp = time.time()
-			print(DetectPIN,"dry")
 			pressed = False
-		time.sleep(0.5)
+		time.sleep(1)
 
 	
 	
 
 if __name__ == '__main__':
 		
-	threading.Thread(target = detect_water ,args = (12,Machine_ENS)).start()
-	threading.Thread(target = detect_water ,args = (16,Machine_ENS)).start()
-	threading.Thread(target = detect_water ,args = (20,Machine_ENS)).start()
-	threading.Thread(target = detect_water ,args = (Machine_PIN,Machine_ENS)).start()
+	threading.Thread(target = detect_water ,args = (Machine1_PIN,Machine1_ENS)).start()
+	threading.Thread(target = detect_water ,args = (Machine2_PIN,Machine2_ENS)).start()
+	threading.Thread(target = detect_water ,args = (Machine3_PIN,Machine3_ENS)).start()
+	threading.Thread(target = detect_water ,args = (Machine4_PIN,Machine4_ENS)).start()
 
 
